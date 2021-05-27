6026562869a2620f2f71542e63a7b27c2ad99e86
Afiz <itsafiz@gmail.com>
2021-05-27 22:58:25 +0530

    templates inheritance

--- app.py
+++ app.py
@@ -9,5 +9,9 @@ def index(name: str = 'world'):
     return render_template('index.html', name=name.capitalize())
 
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=2345)
